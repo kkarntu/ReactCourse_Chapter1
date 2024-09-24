06f0ee2bb121404843c6b19c1afb8bd868a362c4
kkarntu <162441704+kkarntu@users.noreply.github.com>
2024-09-25 00:50:28 +0300

    upd

---
+++ lab1/laba2/src/components/AddToDoForm.jsx
@@ -0,0 +1,19 @@
+function AddToDoForm({ title, handleInputChange, handleSubmit }) {
+    return (
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          value={title}
+          onChange={handleInputChange}
+          name="title"
+          placeholder="Add a task"
+          className="input"
+        />
+        <button type="submit" className="addButton">
+          Add
+        </button>
+      </form>
+    )
+  }
+
+  export default AddToDoForm
---
+++ lab1/laba2/src/components/ToDoItem.jsx
@@ -0,0 +1,12 @@
+function ToDoItem({ id, title, handleDelete }) {
+    return (
+      <li className="listItem">
+        <span>{title}</span>
+        <button onClick={() => handleDelete(id)} className="deleteButton">
+          Delete
+        </button>
+      </li>
+    )
+  }
+
+  export default ToDoItem
